f4c1d00551e7fd0d3db17f1b0769d35dedb790fd
Peter Sagerson <psagers@ignorare.net>
2021-11-29 11:12:45 -0800

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-otp-yubikey',
-    version='1.0.0',
+    version='1.0.1',
     description='A django-otp plugin that verifies YubiKey OTP tokens.',
     author='Peter Sagerson',
     author_email='psagers@ignorare.net',
